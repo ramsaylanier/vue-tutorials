4b9539701e01a18174952b99660c9a35572e2211
Ramsay Lanier <rlanier@ALT8AG8WL.local>
2017-11-28 21:08:46 -0500

    styling updates

--- thumbnail-creator/client/src/components/uploadForm.vue
+++ thumbnail-creator/client/src/components/uploadForm.vue
@@ -1,14 +1,23 @@
 <template>
   <div :class="$style.container">
     <form @submit.prevent="handleSubmit">
-      <input type="file" @change="handleFileChange" multiple/>
+
+      <div :class="$style.input">
+        <input @change="handleFileChange" :class="$style.file" type="file" name="files[]" id="file" data-multiple-caption="{count} files selected" multiple />
+        <label for="file">Choose files</label>
+      </div>
+
       <h3>Files To Upload</h3>
       <ul :class="$style.list">
         <li :class="$style.listItem" v-for="file in files" :key="file.name">
           {{file.name}}
         </li>  
       </ul>
-      <input type="submit" value="Upload"/>
+
+
+      <input v-if="files.length > 0" type="submit" value="Upload" :class="$style.submit"/>
+
+      {{message}}
     </form>
 
   </div>
@@ -20,15 +29,21 @@ export default {
   name: 'upload-form',
   data () {
     return {
-      files: []
+      files: [],
+      message: null
     }
   },
   methods: {
     handleFileChange (e) {
       this.files = [...e.target.files]
+      this.message = null
     },
     handleSubmit (e) {
-      uploadFiles(this.files)
+      uploadFiles(this.files).then(r => {
+        console.log('result:', r)
+        this.files = []
+        this.message = 'Uploaded!'
+      })
     }
   }
 }
@@ -44,11 +59,54 @@ export default {
 
   .list{
     list-style: none;
+    margin: 1rem 0;
+    padding: 0;
   }
 
   .listItem{
+    color: black;
+    display: block;
+    padding: .5rem;
+    border-bottom: 1px solid black;
+  }
+  
+  .input{
+    margin: 1rem 0;
+  }
+
+  .input label{
+    display: inline-block;
+    background-color: black;
+    border: 1px solid black;
+    color: white;
+    padding: 1rem;
+    cursor: pointer;
+    border-radius: 3px;
+  }
+
+  .input label:hover{
+    background-color: white;
+    color: black;
+    cursor: pointer;
+  }
+
+  .file{
+    display: none;
+  }
+
+  .submit{
+    font-size: 1rem;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    border: 1px solid black;
+    background-color: white;
+    padding: 1rem 2rem;
+    border-radius: 3px;
+    transition: background 150ms ease-out;
+  }
+
+  .submit:hover{
     background-color: black;
     color: white;
-    display: block;
+    cursor: pointer;
   }
 </style>
